e73b61e8ab0b0b20ed14aaa5beee8dd3fb150d46
Arnd <git@codefanatic.de>
2022-02-27 18:31:10 +0100

    Improve CMake file structure

--- test/reverse_string_test.cpp
+++ test/reverse_string_test.cpp
@@ -5,9 +5,3 @@ TEST(reverse_string_test, input_string)
 {
     EXPECT_EQ(reverse_string("hello"), "olleh");
 }
-
-int main(int argc, char *argv[])
-{
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
