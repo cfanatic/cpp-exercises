a122654e195ab8cd31f4a55652cf6f5ac312c53a
Arnd <git@codefanatic.de>
2022-02-27 20:20:39 +0100

    Add include guards in header files

--- include/fizz_buzz.hpp
+++ include/fizz_buzz.hpp
@@ -1,4 +1,9 @@
+#ifndef FIZZ_BUZZ_HPP
+#define FIZZ_BUZZ_HPP
+
 #include <iostream>
 #include <string>
 
 void fizz_buzz(int num);
+
+#endif // FIZZ_BUZZ_HPP
--- include/reverse_string.hpp
+++ include/reverse_string.hpp
@@ -1,4 +1,9 @@
+#ifndef REVERSE_STRING_HPP
+#define REVERSE_STRING_HPP
+
 #include <iostream>
 #include <string>
 
 std::string reverse_string(std::string str);
+
+#endif // REVERSE_STRING_HPP
