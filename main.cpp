793df26d1804f75e4e49b9c86f12e0d8d0f0b7f6
Arnd <git@codefanatic.de>
2022-03-06 20:20:52 +0100

    Update main.cpp for linked list exercise

--- main.cpp
+++ main.cpp
@@ -2,8 +2,6 @@
 
 int main()
 {
-    std::cout << "Example of using std::make_shared() and std::shared_ptr()" << std::endl;
-
     std::shared_ptr<ListElement<int>> head = std::make_shared<ListElement<int>>(1337);
 
     auto print = [&]()
@@ -11,10 +9,22 @@ int main()
         std::cout << "head.data: " << head->get_value() << std::endl;
     };
 
+    std::cout << "Head of Linked List:" << std::endl;
     print();
+
+    std::cout << "Inserting two elements before head:" << std::endl;
     head->insert_front(&head, 100);
     print();
     head = head->insert_front(200);
+    print();
+
+    std::cout << "Head of Linked List:" << std::endl;
+    print();
+
+    std::cout << "Printing two elements after head:" << std::endl;
+    head = head->get_next();
+    print();
+    head = head->get_next();
     print();
 
     return 0;
